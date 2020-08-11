fd293802b9e671d58d0d2cffadd9c0d1ba18056c
Srilatha <psrilat2@LAMU02Z339SLVCG.uhc.com>
2020-08-11 21:50:02 +0530

    adding components

---
+++ react-ui/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+
+function Header(props) {
+    return (
+        // <nav class="navbar navbar-dark bg-primary">
+        <>
+            <Container>
+                <strong>Space X Launch Programs</strong>
+            </Container>
+        </>
+    )
+}
+export default Header;
